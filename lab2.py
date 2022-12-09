74bda958aaa10d9f0498402e12f1bc3a876fb752
flexfromhell <118437660+flexfromhell@users.noreply.github.com>
2022-12-09 19:10:19 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,12 +1,22 @@
 import requests
-
-city = "Moscow,RU"
-appid = '89203db4feab1aaaa981e376641c2038'
-res = requests.get('http://api.openweathermap.org/data/2.5/forecast',
-                   params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
-data = res.json()
-print('Прогноз погоды на неделю:')
-for i in data['list']:
-    print()
-    print('Дата <', i['dt_txt'], '> \r\nВидимость <', '{0:+3.0f}'.format(i['visibility']), 'm' '> \r\nСкорость ветра <',
-          i['wind']['speed'], 'm/s >')
+appid = "89203db4feab1aaaa981e376641c2038" 
+city = "Moscow"
+def weather_today():
+	res = requests.get("http://api.openweathermap.org/data/2.5/weather", params={'q': city,'units':'metric', 'lang':'ru', 'APPID':appid})
+	data = res.json()
+	print("Город:", city)
+	print("Погодные условия:", data['weather'][0]['description'])
+	print("Температура:", data['main']['temp'])
+	print("Минимальная температура:", data['main']['temp_min'])
+	print("Максимальная температура:", data['main']['temp_max'])
+	print("Скорость ветра:", data['wind']['speed'])
+	print("Видимость:", data['visibility'])
+def weather_week():
+	res = requests.get("http://api.openweathermap.org/data/2.5/forecast", params={'q': city,'units':'metric', 'lang':'ru', 'APPID':appid})
+	data = res.json()
+	print("Прогноз погоды на неделю:")
+	for i in data['list']:
+		print("Дата <", i['dt_txt'], "> \r\nТемпература <",'{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <", i['weather'][0]['description'], ">", "\r\nСкорость ветра <", i["wind"]["speed"], ">", "\r\nВидимость <", i["visibility"], ">")
+		print("____________________________")
+weather_week()
+#weather_today()
